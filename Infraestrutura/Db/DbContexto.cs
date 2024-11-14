a846574690c394546ebd9f0bafed2215a15f4aa9
israelGitV1 <kasper.war@gmail.com>
2024-11-14 09:15:20 -0300

    :sparkles: feat(DbContext): add class DbContexto

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,13 +1,39 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MinimalApi.Dominio.Entidades;
 
 namespace MinimalApi.Infraestrutura.Db
 {
     public class DbContexto : DbContext
     {
-        
+        private readonly IConfiguration  _ConfiguracaoAppSettings;
+
+        public DbContexto (IConfiguration configurationAppSettings)
+        {
+            _ConfiguracaoAppSettings = configurationAppSettings;
+        }
+
+        public DbSet<Administrador> Administradores {get; set;} = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            if(!optionsBuilder.IsConfigured)
+            {
+                var stringConexao = _ConfiguracaoAppSettings.GetConnectionString("MySql")?.ToString();
+
+                if(!string.IsNullOrEmpty(stringConexao))
+                {
+                    optionsBuilder.UseMySql(stringConexao,ServerVersion.AutoDetect(stringConexao));
+                }
+            }
+        }
+    
+
     }
 }
