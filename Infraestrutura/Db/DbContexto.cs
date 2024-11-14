3b5e334a1a9162e629e61823e3fb28020ba94b70
israelGitV1 <kasper.war@gmail.com>
2024-11-14 08:09:49 -0300

    :heavy_plus_sign: build: Instalando dependencias

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MinimalApi.Infraestrutura.Db
 {
-    public class DbContexto
+    public class DbContexto : DbContext
     {
         
     }
