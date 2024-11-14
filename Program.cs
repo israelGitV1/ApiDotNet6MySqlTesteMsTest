bb731a75f0010cfd0fd57baf5743c62a8f98ba8a
israelGitV1 <kasper.war@gmail.com>
2024-11-14 09:33:28 -0300

    :sparkles: feat(Program.cs): addDbCotext<DbContexto> e GetConnection:MySql

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using MinimalApi.Dominio.DTOs;
+using MinimalApi.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-List<ContaDTO> conta = new List<ContaDTO>();
+
+var stringMySql = builder.Configuration.GetConnectionString("MySql");
+builder.Services.AddDbContext<DbContexto>(options => 
+{
+  options.UseMySql(stringMySql,ServerVersion.AutoDetect(stringMySql));
+});
 
 app.MapGet("/", () => "Hello World!");
 
@@ -15,25 +22,5 @@ app.MapPost("/login",(LoginDTO loginDTO) =>
         return Results.Unauthorized();
 });
 
-app.MapPost("/cadastro",(ContaDTO contaDTO) =>
-{
-  conta.Add(contaDTO);
-  return Results.Ok("Cadastro com Sucesso!");
-});
-app.MapGet("/contas",() =>
-{
-  if(conta.Count > 0)
-    return Results.Ok(conta.ToList());
-  else
-    return Results.NoContent();
-});
-
 app.Run();
 
-public class ContaDTO
-{
-    public string Nome {get; set;} = default!;
-    public string Email {get; set;} = default!;
-
-    public string Senha {get; set;} = default!;
-}
