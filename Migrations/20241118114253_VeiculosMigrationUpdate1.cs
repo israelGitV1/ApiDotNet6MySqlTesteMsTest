a3c2a95276993f4830f92bc84b4d65558bbea109
israelGitV1 <kasper.war@gmail.com>
2024-11-18 11:03:24 -0300

    :sparkles: feat(Migration): VeiculosMigration_VeiculoUpdate1

---
+++ Migrations/20241118114253_VeiculosMigrationUpdate1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalApi.Migrations
+{
+    public partial class VeiculosMigrationUpdate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Ano",
+                table: "Veiculos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Marca",
+                table: "Veiculos",
+                type: "varchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ano",
+                table: "Veiculos");
+
+            migrationBuilder.DropColumn(
+                name: "Marca",
+                table: "Veiculos");
+        }
+    }
+}
