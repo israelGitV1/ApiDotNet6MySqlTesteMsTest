a95ae28abfb541151ecf2a0f0a4be272f48b2427
israelGitV1 <kasper.war@gmail.com>
2024-11-21 10:21:58 -0300

    :sparkles: feat(Servicos): ImplementacaoDaInterfaceIAdministradorServico_Em_AdiministradorServico

--- Dominio/Servicos/AdministradorServico.cs
+++ Dominio/Servicos/AdministradorServico.cs
@@ -19,10 +19,37 @@ namespace MinimalApi.Dominio.Servicos
             _Contexto = contexto;
         }
 
+        public void Incluir(Administrador administrador)
+        {
+            _Contexto.Add(administrador);
+            _Contexto.SaveChanges();
+        }
+
         public Administrador? Login(LoginDTO loginDTO)
         {
             var adm = _Contexto.Administradores.Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha).FirstOrDefault();
             return adm;
+        }
+
+        public List<Administrador>? BuscarTodos (int pagina = 1 )
+        {
+            var administradores = _Contexto.Administradores.ToList();
+            var numeroPorPagina = 10;
+            var administradoresPagina = administradores.Skip((pagina-1)*numeroPorPagina).Take(numeroPorPagina).ToList();
+
+            return administradoresPagina;
+        }
+
+        public bool Excluir(LoginDTO loginDTO)
+        {
+            var administrador = _Contexto.Administradores.Where(x => x.Email == loginDTO.Email && x.Senha == loginDTO.Senha).FirstOrDefault();
+            if(administrador != null)
+            {
+                _Contexto.Administradores.Remove(administrador);
+                _Contexto.SaveChanges();
+                return true;
+            }
+                return false;
         }
     }
 }
