3bab7c0a9f971227a3a7db77267fd4a1843e85aa
israelGitV1 <kasper.war@gmail.com>
2024-11-21 10:38:01 -0300

    :sparkles: feat(Program): addCRUDAdministradores_ErrosDeValidacaoValidaLoginDTO_ErrosDeValidacaoValidaAdministradorDTO_AddWithTagsDoSwagger

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Security.Cryptography;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
@@ -14,38 +15,103 @@ using MinimalApi.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var stringMySql = builder.Configuration.GetConnectionString("MySql");
-builder.Services.AddDbContext<DbContexto>(options => 
-{
-  options.UseMySql(stringMySql,ServerVersion.AutoDetect(stringMySql));
-});
-
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico> ();
 builder.Services.AddScoped<IVeiculoServico, VeiculoServico> ();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var stringMySql = builder.Configuration.GetConnectionString("MySql");
+builder.Services.AddDbContext<DbContexto>(options => 
+{
+  options.UseMySql(stringMySql,ServerVersion.AutoDetect(stringMySql));
+});
+
 var app = builder.Build();
 
 #endregion
 
 #region Home
-
-app.MapGet("/", () => Results.Json(new Home()));
-
+app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
 #endregion
 
 #region Administrador
 
+ErrosDeValidacao ValidaLoginDTO (LoginDTO loginDTO)
+{
+  var validacao = new ErrosDeValidacao { Mensagens = new List<string>()};
+  if(String.IsNullOrEmpty(loginDTO.Email))
+    validacao.Mensagens.Add("Email não pode ficar em branco. ");
+  if(String.IsNullOrEmpty(loginDTO.Senha))
+    validacao.Mensagens.Add("Senha invalida. ");
+
+   return validacao; 
+}
+ErrosDeValidacao ValidaAdministradorDTO (AdministradorDTO administradorDTO)
+{
+  var validacao = new ErrosDeValidacao { Mensagens = new List<string>() };
+  if(String.IsNullOrEmpty(administradorDTO.Email))
+    validacao.Mensagens.Add("Email não pode ficar em branco. ");
+  if(String.IsNullOrEmpty(administradorDTO.Senha))
+    validacao.Mensagens.Add("Senha invalida. ");
+  if(String.IsNullOrEmpty(administradorDTO.Perfil))
+    validacao.Mensagens.Add("Perfil não pode ficar em branco. ");
+
+   return validacao; 
+}
+
 app.MapPost("/login",([FromBody] LoginDTO loginDTO , IAdministradorServico administradorServico) =>
 {
+  var validacao =  ValidaLoginDTO(loginDTO);
+  if(validacao.Mensagens.Count > 0)
+    return Results.BadRequest(validacao);
+
     if(administradorServico.Login(loginDTO) != null)
         return Results.Ok("Login com sucesso");
     else
         return Results.Unauthorized();
-});
+})
+.WithTags("Administradores");
 
+app.MapPost("/administrador",([FromBody] AdministradorDTO administradorDTO, IAdministradorServico administradorServico) =>
+{
+  var validacao =  ValidaAdministradorDTO(administradorDTO);
+  if(validacao.Mensagens.Count > 0)
+    return Results.BadRequest(validacao);
+  
+  var administrador = new Administrador
+  {
+    Email = administradorDTO.Email,
+    Senha = administradorDTO.Senha,
+    Perfil = administradorDTO.Perfil,
+  };
+  administradorServico.Incluir(administrador);
+  return Results.Created($"/administrador/{administrador.Id}",administrador);
+
+})
+.WithTags("Administradores");
+
+app.MapDelete("/administrador",([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico)=>
+{
+  var validacao = ValidaLoginDTO(loginDTO);
+  if( validacao.Mensagens.Count > 0)
+    return Results.BadRequest(validacao);
+
+  administradorServico.Excluir(loginDTO);
+  return Results.NoContent();
+})
+.WithTags("Administradores");
+
+app.MapGet("/administrador",([FromQuery] int? pagina ,IAdministradorServico administradorServico) =>
+{
+  int NumPagina = pagina > 0 ? Convert.ToInt32(pagina) : 1 ;
+  var administradores = administradorServico.BuscarTodos(NumPagina);
+  if(administradores == null)
+    return Results.NotFound();
+
+  return Results.Ok(administradores);
+})
+.WithTags("Administradores");
 #endregion
 
 #region Veiculos
@@ -83,14 +149,16 @@ app.MapPost("/veiculo",([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veicul
   veiculoServico.Incluir(veiculo);
   return Results.Created($"/veiculo/{veiculo.Id}",veiculo);
 
-});
+})
+.WithTags("Veiculos");
 
 app.MapGet("/veiculos",([FromQuery] int? pagina, IVeiculoServico veiculoServico ) => 
 { 
   int pg = pagina > 1 ? Convert.ToInt32(pagina) : 1;
     var veiculos = veiculoServico.Todos(pg);
     return Results.Ok(veiculos);
-}).WithTags("veiculos");
+})
+.WithTags("Veiculos");
 
 app.MapGet("/veiculo/{id}",([FromRoute] int id , IVeiculoServico veiculoServico) =>
 {
@@ -103,7 +171,8 @@ app.MapGet("/veiculo/{id}",([FromRoute] int id , IVeiculoServico veiculoServico)
   }
   else
     return Results.NotFound();
-}).WithTags("veiculos");
+})
+.WithTags("Veiculos");
 
 app.MapPut("/veiculo/{id}",([FromRoute] int id, VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) => 
 {
@@ -123,7 +192,8 @@ app.MapPut("/veiculo/{id}",([FromRoute] int id, VeiculoDTO veiculoDTO, IVeiculoS
   veiculoServico.Atualizar(veiculo);
   return Results.Ok(veiculo);
 
-}).WithTags("veiculos");
+})
+.WithTags("Veiculos");
 
 app.MapDelete("veiculo/{id}",([FromRoute] int id , IVeiculoServico veiculoServico) =>
 {
@@ -133,13 +203,15 @@ app.MapDelete("veiculo/{id}",([FromRoute] int id , IVeiculoServico veiculoServic
   veiculoServico.ApagarPorId(veiculo);
   return Results.NoContent();
 
-}).WithTags("veiculos");
+})
+.WithTags("Veiculos");
 #endregion
 
 #region App
 
+
 app.UseSwagger();
 app.UseSwaggerUI();
-app.Run();
 
+app.Run();
 #endregion
